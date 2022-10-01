3e4e5169a1d1164aa26ab7a6239461ba14f9e635
Gabriel Golzman <ggolzman@primerosystems.com>
2022-09-30 21:00:25 -0300

    Class 2

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [recipeText, setRecipeText] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const recipeInputHandler = (enteredText) => {
+    setRecipeText(enteredText);
+  };
+
+  const addRecipeHandler = () => {
+    setRecipes((oldRecipes) => [...oldRecipes, recipeText]);
+    setRecipeText("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          value={recipeText}
+          placeholder="Ingrese el nombre de la receta"
+          onChangeText={recipeInputHandler}
+        />
+        <View style={styles.buttonInput}>
+          <Button onPress={addRecipeHandler} title="Agregar receta" />
+        </View>
+      </View>
+      <View>
+        {recipes.map((recipe, index) => (
+          <Text style={styles.recipeItem} key={index}>
+            {recipe}
+          </Text>
+        ))}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  inputContainer: {
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "100%",
+    marginRight: 8,
+    marginVertical: 15,
+    padding: 8,
+  },
+  buttonInput: {
+    width: "50%",
+    alignSelf: "flex-end",
+  },
+  recipeItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#CB6011",
+    color: "white",
   },
 });
+
+export default App;
